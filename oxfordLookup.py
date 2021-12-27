22d2a68dec3b842bac43eb77754069a0e966ad6b
Muhammad Ali <kamoliddeenov@gmail.com>
2021-12-27 15:12:59 +0500

    functions

---
+++ oxfordLookup.py
@@ -0,0 +1,30 @@
+import requests
+
+app_id = "ee095ac" # Oxford Dictionary APP ID
+app_key = "cd1b6cf35dd14da42e9f5cb9e47f82c" # Oxford Dictionary API KEY
+language = "en-gb"
+
+def getDefinitions(word_id):
+    url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
+    r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+    res = r.json()
+    if 'error' in res.keys():
+        return False
+
+    output = {}
+    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
+    definitions = []
+    for sense in senses:
+        definitions.append(f"👉 {sense['definitions'][0]}")
+
+    output['definitions'] = "\n".join(definitions)
+
+    if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get('audioFile'):
+        output['audio'] = res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
+
+    return output
+
+if __name__ == "__main__":
+    from pprint import pprint as print
+    print(getDefinitions('orange'))
+    print(getDefinitions('Great Britain'))
